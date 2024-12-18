ec85dff65af91760b7ceb9fa0ad3e8d72d49ba97
denismusoev <denis.musoev@yandex.ru>
2024-12-19 03:46:24 +0400

    Фик бага создания объявления

--- clickboard-backend/src/main/java/com/coursework/clickboardbackend/ad/service/AdService.java
+++ clickboard-backend/src/main/java/com/coursework/clickboardbackend/ad/service/AdService.java
@@ -135,18 +135,20 @@ public class AdService {
             }
         }
 
-        if (!photoUrls.isEmpty()) {
+        if (photoUrls.isEmpty()) {
             AdPhoto photo = new AdPhoto();
             photo.setPhotoUrl("https://res.cloudinary.com/dionvmulc/image/upload/v1734510457/9044950_no_image_icon_glykxw.png");
             photo.setAd(finalAd);
             adPhotoRepository.save(photo);
         }
-        photoUrls.forEach(photoUrl -> {
-            AdPhoto photo = new AdPhoto();
-            photo.setPhotoUrl(photoUrl);
-            photo.setAd(finalAd);
-            adPhotoRepository.save(photo);
-        });
+        else {
+            photoUrls.forEach(photoUrl -> {
+                AdPhoto photo = new AdPhoto();
+                photo.setPhotoUrl(photoUrl);
+                photo.setAd(finalAd);
+                adPhotoRepository.save(photo);
+            });
+        }
 
         updateAdStatus(ad.getId(), Ad.Status.PENDING);
 
